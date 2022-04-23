8a3ff3bac254bbc5ad9898abe99f2c7fbe650f26
Lucho-LJA <ljallauca.a@gmail.com>
2022-04-22 23:01:30 -0500

    Add basic operations to calculator and test to them

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,14 @@
 class Calculator
-    def add(a,b)
-        a + b
+    def add(*elements)
+        elements.inject{|sum, elm| sum += elm}
+    end
+    def subt(a,b)
+        a - b
+    end
+    def mult(*elements)
+        elements.inject(1){|mul, elm| mul *= elm}
+    end
+    def div(a,b)
+        a.to_f / b.to_f
     end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,9 +1,35 @@
 require './lib/calculator'
 describe Calculator do
     describe "#add" do
-      it "returns the sum of two numbers" do
-        calculator = Calculator.new
-        expect(calculator.add(5, 2)).to eql(7)
-      end
+        it "returns the sum of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.add(5, 2)).to eql(7)
+        end
+        it "returns the sum of more than two numbers" do
+            calculator = Calculator.new
+            expect(calculator.add(2, 5, 7)).to eql(14)
+        end
+    end
+    describe "#subtract" do
+        it "returns the subtract of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.subt(5, 2)).to eql(3)
+        end
+    end
+    describe "#multiply" do
+        it "returns the multiplication of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.mult(5, 2)).to eql(10)
+        end
+        it "returns the multiplication of three numbers" do
+            calculator = Calculator.new
+            expect(calculator.mult(2, 5, 7)).to eql(70)
+        end
+    end
+    describe "#divide" do
+        it "returns the divition of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.div(5, 2)).to eql(2.5)
+        end
     end
 end
